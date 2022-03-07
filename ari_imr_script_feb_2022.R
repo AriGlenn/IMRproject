d3a48518fd48184ea34b2811ecf01e169805d589
AriGlenn <ariisawesome22@gmail.com>
2022-03-07 15:59:00 -0800

    Update imr script (comment code, add var for num villages)

--- ari_imr_script_feb_2022.R
+++ ari_imr_script_feb_2022.R
@@ -1,79 +1,104 @@
-setwd()
+setwd("/Users/ariglenn/Desktop/Ratledge Work/IMRproject")
 
-#not sure we still need all these, but...
-library(raster)
-library(MetBrewer)
-library(sf)
+library(raster) #extract rasters
+library(MetBrewer) #color palet
+library(sf) #helps switch from different type of shape files
 library(tidyverse)
-library(rgdal)
-library(fixest)
-library(ggplot2)
-library(utils)
 library(dplyr)
 library(fixest)
-library(reshape2)
-library(lfe)
-library(foreign) 
-#library(MCPanel) 
-library(glmnet)
-library(ggthemes)
+library(glmnet) #machine learning weighted regression
+library(ggplot2) #plotter
+library(ggthemes) #makes figures look nice
+library(reshape2) #reshapes data
+library(utils)
+library(foreign) #interpret different types of data frames
+library(MCPanel) #matrix completion
+
+#might be able to remove
+#library(rgdal)
+#library(lfe)
 
 
 # loading treatment and control data
 TC_2km <- read.csv("TC_2km.csv") #csv is in the drive
 
-TC_2km_2014sub <- subset(TC_2km, year == "2014") #964 total = 888 control and 76 treated
+#takes data from all rows w/2014 as a data value under year by View(TC_2km)
+#964 total = 888 control and 76 treated
+TC_2km_2014sub <- subset(TC_2km, year == "2014")
 
-count <- matrix(NA,964,1)
-count[,1] <- c(1:964)
+#creates a matrix with 964 rows, 1 col, filled w/NA
+#number of rows = length of data = number of villages
+numVillages <- 964
+count <- matrix(NA,numVillages,1)
+#labels each row from 1 to 964
+count[,1] <- c(1:numVillages)
 
+#assigns each row in the data set of the 2014 subset with a unqiue identifies (1-964)
 TC_2km_2014sub_count <- cbind(count,TC_2km_2014sub)
 
-TC_2km_2014sub_count <- TC_2km_2014sub_count[,c(5,4)] #needs to be long, lat
+#deletes everything except the longitude and latitude values from the data set (from columns 4 and 5)
+#needs to be long, lat
+TC_2km_2014sub_count <- TC_2km_2014sub_count[,c(5,4)]
 
 # here is how to load and prep infant mortality data i just sent
 
-e <- as(extent(29, 34, -2, 5), 'SpatialPolygons') #this is just setting the extent of the raster we want to extract
+#creates a box of latitude and longitude that roughly focus on Uganda
+#this is just setting the extent of the raster we want to extract
 # i.e. just lat and long box around uganda, not the whole world!
+e <- as(extent(29, 34, -2, 5), 'SpatialPolygons')
+
+#ensures that all data is in the same coordinate system
 crs(e) <- "EPSG:4326"
 
+#imports TIF file that has many layers with stack
+#infant data from ghdx data set
 ghdx_infant <- stack("IHME_LMICS_U5M_2000_2017_D_INFANT_MEAN_Y2019M10D16.TIF")
+
+#gets infant data from the defined Uganda box (e)
 ghdx_trial_ug <- crop(ghdx_infant, e)
+
+#ensures that all data is in the same coordinate system
 pr_ghdx_trial_ug <- projectRaster(ghdx_trial_ug,crs="+proj=longlat +datum=NAD27")
 
+#from the TIF file, extracts the points related to the TC_2km_2014sub_count
+#gets infant data related to each village (coordinates for each TC_2km_2014sub_count -- 964 villages)
 rast_pr_ghdx_trial_ug <- raster::extract(pr_ghdx_trial_ug, TC_2km_2014sub_count)
+
+#ensures that all data is in the same data frame
 rast_pr_ghdx_trial_ug <- as.data.frame(rast_pr_ghdx_trial_ug)
 
+#renames each column for years 2000-2017
 colnames(rast_pr_ghdx_trial_ug) <- c("imr_2000", "imr_2001", "imr_2002", "imr_2003", "imr_2004", 
                                      "imr_2005", "imr_2006", "imr_2007", "imr_2008", "imr_2009", 
                                      "imr_2010", "imr_2011", "imr_2012", "imr_2013", "imr_2014", 
                                      "imr_2015", "imr_2016", "imr_2017")
 
-############
-# additions on feb 23 2022
-###########
+#matrix that contains imr for years 2000-2017 for each 964 villages in Uganda
+
 
 # this is the website for the nighttime light data. 
 # https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/YGIVCD
 
-count <- matrix(NA,964,1) # just creating a unique identifier
-count[,1] <- c(1:964)
-
+count <- matrix(NA,numVillages,1) # just creating a unique identifier
+count[,1] <- c(1:numVillages)
 
 # now starting the raster extract for 2003 to 2016
 
 NTL_2003 <- raster("LongNTL_2003.tif")
 ug_NTL_2003 <- crop(NTL_2003, e)
 pr_NTL_2003 <- projectRaster(ug_NTL_2003,crs="+proj=longlat +datum=NAD27")
-plot(pr_NTL_2003) # just ot spot check there are not import problems
+# plot just to spot check that there are not import problems
+plot(pr_NTL_2003)
 
 rast_val_NTL_2003_TC <- raster::extract(pr_NTL_2003, TC_2km_2014sub_count)
 rast_val_NTL_2003_TC <- as.data.frame(rast_val_NTL_2003_TC)
 colnames(rast_val_NTL_2003_TC) <- "ntl_2003"
 
-write.csv(rast_val_NTL_2003_TC, "rast_val_NTL_2003_TC.csv") # this save the data so we don't have to do the extraction again. 
+# this save the data so we don't have to do the extraction again
+write.csv(rast_val_NTL_2003_TC, "rast_val_NTL_2003_TC.csv")
 
-raster_TC_2km_2014sub <- cbind(TC_2km_2014sub, rast_val_NTL_2003_TC) # this combines the extracted data to the original data frame
+# this combines the extracted data to the original data frame
+raster_TC_2km_2014sub <- cbind(TC_2km_2014sub, rast_val_NTL_2003_TC)
 
 # 2004
 NTL_2004 <- raster("LongNTL_2004.tif")
